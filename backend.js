9219a08567c8ec036fe92f6eeaaf4cff3f3813bb
Napat Nueyen <s6406021622036@email.kmutnb.ac.th>
2025-03-13 21:38:21 +0700

    Update database URL in backend.js to include database name

--- backend.js
+++ backend.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(express.json());
 
 // set db url
-const dbUrl = 'postgres://webadmin:XYIcvc98762@node77385-netcafe-project.proen.app.ruk-com.cloud:11678';
+const dbUrl = 'postgres://webadmin:LTIfke85193@node77385-netcafe-project.proen.app.ruk-com.cloud:11678/Netcafe';
 
 // create a connection to the database
 const sequelize = new Sequelize(dbUrl);
