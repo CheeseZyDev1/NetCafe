b7611d259a119e833f0c3449831cf157ff981411
Napat Nueyen <s6406021622036@email.kmutnb.ac.th>
2025-03-13 10:13:53 +0700

    Update database URL and change default port in backend.js

--- backend.js
+++ backend.js
@@ -29,7 +29,7 @@ const app = express();
 app.use(express.json());
 
 // set db url
-const dbUrl = 'postgres://webadmin:XYIcvc98762@node71725-noderest-67.proen.app.ruk-com.cloud:11749/login';
+const dbUrl = 'postgres://webadmin:XYIcvc98762@node71725-noderest-67.proen.app.ruk-com.cloud:11749/netcafe';
 
 // create a connection to the database
 const sequelize = new Sequelize(dbUrl);
@@ -49,7 +49,7 @@ const {
 
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3500;
 
 // ใช้งาน Middleware
 app.use(cors());
