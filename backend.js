a903b26119f4f32419d8f8dab5dbb4cfdbc83c14
Napat Nueyen <s6406021622036@email.kmutnb.ac.th>
2025-03-17 22:03:24 +0700

    Update database connection settings and add .env file for configuration

--- backend.js
+++ backend.js
@@ -29,9 +29,9 @@ const app = express();
 app.use(express.json());
 
 // set db url
-const dbUrl = "postgresql://postgres:HzG4tTsbQ6DnaX8w@db.ejxgohnorvvrnfmqiymv.supabase.co:5432/postgres"
+//const dbUrl = "postgresql://postgres:HzG4tTsbQ6DnaX8w@db.ejxgohnorvvrnfmqiymv.supabase.co:5432/postgres"
 //const dbUrl = 'postgres://webadmin:LTIfke85193@10.104.21.86/Netcafe';
-//const dbUrl = 'postgres://webadmin:LTIfke85193@node77385-netcafe-project.proen.app.ruk-com.cloud:11678/Netcafe';
+const dbUrl = 'postgres://webadmin:LTIfke85193@node77385-netcafe-project.proen.app.ruk-com.cloud:11678/Netcafe';
 
 // create a connection to the database
 const sequelize = new Sequelize(dbUrl);
